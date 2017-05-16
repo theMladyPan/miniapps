afa007d33ec4f785327f9f797d222dc3ced60642
theMladyPan <stanislav@rubint.sk>
2017-05-16 23:00:32 +0200

    discover.py paralellized

--- discover.py
+++ discover.py
@@ -1,14 +1,42 @@
 #Boa:Frame:Frame1
 
-import wx, os, subprocess, sys, webbrowser
+import wx, os, subprocess, sys, webbrowser, time
+from multiprocessing import Process, Queue, cpu_count
 
 def create(parent):
     return Frame1(parent)
 
-[wxID_FRAME1, wxID_FRAME1COUNTDOWN, wxID_FRAME1DISCOVER, 
- wxID_FRAME1ENTRY_SUBNET, wxID_FRAME1GAUGE, wxID_FRAME1LIST_NODES, 
- wxID_FRAME1STATICBOX1, wxID_FRAME1STATICBOX2, 
+[wxID_FRAME1, wxID_FRAME1COUNTDOWN, wxID_FRAME1DISCOVER,
+ wxID_FRAME1ENTRY_SUBNET, wxID_FRAME1GAUGE, wxID_FRAME1LIST_NODES,
+ wxID_FRAME1STATICBOX1, wxID_FRAME1STATICBOX2,
 ] = [wx.NewId() for _init_ctrls in range(8)]
+
+class MultiP(Process):
+    def __init__(self, queue, prefix,i, devnull):
+        Process.__init__(self)
+
+        self.queue=queue
+        self.prefix=prefix
+        self.i=i
+        self.devnull=devnull
+        self.start()
+    def run(self):
+
+        try:
+            if os.name=="nt":
+                subprocess.check_call(['ping','/n','1','/w','50','%s%d'%(self.prefix,self.i)], stdout=self.devnull, stderr=subprocess.STDOUT, shell=True)
+            elif os.name=="posix":
+                subprocess.check_call(['ping -c1 -w1 %s%d'%(self.prefix,self.i)], stdout=self.devnull, stderr=subprocess.STDOUT, shell=True)
+            else:
+                 pass
+
+            self.queue.put('%s%d'%(self.prefix,self.i))
+
+        except subprocess.CalledProcessError:
+            self.queue.put(chr(0))
+        except:
+            print "ERROR"
+
 
 class Frame1(wx.Frame):
     def _init_ctrls(self, prnt):
@@ -24,7 +52,7 @@ class Frame1(wx.Frame):
 
         self.entry_subnet = wx.TextCtrl(id=wxID_FRAME1ENTRY_SUBNET,
               name=u'entry_subnet', parent=self, pos=wx.Point(16, 32),
-              size=wx.Size(216, 32), style=0, value=u'192.168.43.x')
+              size=wx.Size(216, 32), style=0, value=u'192.168.1.x')
 
         self.gauge = wx.Gauge(id=wxID_FRAME1GAUGE, name=u'gauge', parent=self,
               pos=wx.Point(16, 72), range=255, size=wx.Size(216, 24),
@@ -61,23 +89,25 @@ class Frame1(wx.Frame):
         prefix=self.entry_subnet.GetValue().replace("x","")
         wx.Yield()
         with open(os.devnull, 'wb') as devnull:
+            queue=Queue()
             for i in range(1,255):
-                try:
-                    self.entry_subnet.SetValue("%s%d"%(prefix,i))
-                    if os.name=="nt":
-                        subprocess.check_call(['ping','/n','1','/w','50','%s%d'%(prefix,i)], stdout=devnull, stderr=subprocess.STDOUT, shell=True)
-                    elif os.name=="posix":
-                        subprocess.check_call(['ping -c1 -w1 %s%d'%(prefix,i)], stdout=devnull, stderr=subprocess.STDOUT, shell=True)
-                    else:
-                         pass
-                    self.list_nodes.Insert("%s%d"%(prefix,i), len(self.list_nodes.GetItems()))
+                self.entry_subnet.SetValue("%s%d"%(prefix,i))
 
-                except subprocess.CalledProcessError:
-                    pass
+                MultiP(queue, prefix, i, devnull)
+
                 self.gauge.SetValue(i)
                 self.countdown.Label="Approx. time: %02d:%02d"%((254-i)/60, (254-i)%60)
                 wx.Yield()
-        
+            count=254
+
+            while count:
+                result=queue.get()
+                if result != chr(0):
+                    self.list_nodes.Insert(queue.get(), len(self.list_nodes.GetItems()))
+                    count -= 2
+                if result == chr(0):
+                    count -=1
+
         self.entry_subnet.SetValue("%sx"%prefix)
         self.discover.Enable()
         self.entry_subnet.Enable()
